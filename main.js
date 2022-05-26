6c1054d1d686cb01edd67fde99ed04db9c5cd2b8
Avani-4-4-10 <92733721+Avani-4-4-10@users.noreply.github.com>
2022-05-26 18:39:54 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,32 @@
-function setup(){
-   video = createCapture(VIDEO)
-   video.size(530 , 400)
-    canvas = createCanvas(530 , 400)
-   classifier= ml5.poseNet(video , modelLoaded)
-   classifier.on("pose" , gotPoses )
+var leftWristx = 0
+var rightWristx = 0
+var difference=0
+
+function setup() {
+    video = createCapture(VIDEO)
+    video.size(530, 400)
+    canvas = createCanvas(530, 400)
+    classifier = ml5.poseNet(video, modelLoaded)
+    classifier.on("pose", gotPoses)
 }
-function draw(){
+
+function draw() {
     background("grey")
+    textSize(difference)
+    fill("red")
+    text("Avani ", 100, 200)
 }
-function modelLoaded(){
+
+function modelLoaded() {
     console.log("PoseNet is successfully initialized.")
 }
-function gotPoses(results){
-if(results.length > 0){
-    console.log(results)
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results)
+        leftWristx=results[0].pose.leftWrist.x
+        rightWristx=results[0].pose.rightWrist.x
+        difference = floor(leftWristx - rightWristx)
+    
+    }
 }
-  }
